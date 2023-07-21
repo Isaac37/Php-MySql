bad5f299e5bc92ebb02c079ce5d1920693846755
Isaac Kumbula <isaack@fordsworth.com>
2023-07-21 10:27:01 +0200

    modified:   includes/footer.php 	modified:   includes/header.php

--- includes/header.php
+++ includes/header.php
@@ -4,22 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>Vision3 -<?php echo $title  ?></title>
 </head>
 
 <body>
+<div class="container">
     <h2>Follow each link to the page example</h2>
   
-    <ul>
-        <li> <a href="index.php">Home</a></li>
-        <li> <a href="functions.php">functions</a></li>
-        <li> <a href="array.php">Array Statement</a></li>
-        <li> <a href="datetime.php">Date time manipulation</a></li>
-        <li> <a href="stringmanipulation.php">STRING mANIPULATIONt</a></li>
-        <li> <a href="forloop.php">For Loop</a></li>
-        <li> <a href="whileloop.php">While/ Do While Loop</a></li>
-        <li> <a href="ifstatement.php">If statement</a></li>
-        <li> <a href="switchstatement.php">Switch Statement</a></li>
-        
+
+    <ul class="navbar-nav">
+        <li class="nav-item"> <a class="nav-link" href="index.php">Home</a></li>
+        <li class="nav-item"> <a class="nav-link" href="functions.php">functions</a></li>
+        <li class="nav-item"> <a class="nav-link" href="array.php">Array Statement</a></li>
+        <li class="nav-item"> <a class="nav-link" href="datetime.php">Date time manipulation</a></li>
+        <li class="nav-item"> <a class="nav-link" href="stringmanipulation.php">STRING mANIPULATIONt</a></li>
+        <li class="nav-item"> <a class="nav-link" href="forloop.php">For Loop</a></li>
+        <li class="nav-item"> <a class="nav-link" href="whileloop.php">While/ Do While Loop</a></li>
+        <li class="nav-item"> <a class="nav-link" href="ifstatement.php">If statement</a></li>
+        <li class="nav-item"> <a class="nav-link" href="switchstatement.php">Switch Statement</a></li>
+
     </ul>
-    </ul>
+   
